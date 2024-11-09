bad56f35ff726520ffa916432df54848377a489f
ivanShar <oldrum75@gmail.com>
2024-11-10 02:19:18 +0600

    feat: save progress

--- src/keyboardButton/KeyboarButton.tsx
+++ src/keyboardButton/KeyboarButton.tsx
@@ -2,24 +2,28 @@ import styles from './KeyboardButton.module.scss'
 import { useState } from 'react'
 
 const KeyboardButton = () => {
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(false)
 
   const handleClick = () => {
-    setIsActive(!isActive);
-  };
+    setIsActive(!isActive)
+  }
 
   return (
     <div className={styles.buttonContainerAll}>
-    <button 
-      title='1' 
-      className={`${styles.buttonContainer} ${isActive ? styles.active : ''}`}
-      onClick={handleClick}
-    >
-      <div className={`${styles.button} ${isActive ? styles.active : ''}`}>
-        <div className={styles.buttonPressZone}></div>
-      </div>
-    </button>
-    <div className={`${styles.buttonIndicator} ${isActive ? styles.active : ''}`} />
+      <button title="1" className={`${styles.buttonContainer} ${isActive ? styles.active : ''}`} onClick={handleClick}>
+        <div className={`${styles.button} ${isActive ? styles.active : ''}`}>
+          <div className={styles.buttonPressZone}>
+            <div className={styles.buttonImageContainer}>
+              <div className={styles.buttonImage}>
+                <svg width="36px" height="36px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M7 17L17 7L12 2V22L17 17L7 7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </button>
+      <div className={`${styles.buttonIndicator} ${isActive ? styles.active : ''}`} />
     </div>
   )
 }
