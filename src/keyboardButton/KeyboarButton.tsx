011afac989d73ff477b23fc9da4a72255399592c
ivanShar <oldrum75@gmail.com>
2024-11-10 01:45:53 +0600

    feat: save progress

--- src/keyboardButton/KeyboarButton.tsx
+++ src/keyboardButton/KeyboarButton.tsx
@@ -1,11 +1,25 @@
 import styles from './KeyboardButton.module.scss'
+import { useState } from 'react'
 
 const KeyboardButton = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <div className={styles.buttonContainer}>
-      <div className={styles.button}>
+    <div className={styles.buttonContainerAll}>
+    <button 
+      title='1' 
+      className={`${styles.buttonContainer} ${isActive ? styles.active : ''}`}
+      onClick={handleClick}
+    >
+      <div className={`${styles.button} ${isActive ? styles.active : ''}`}>
         <div className={styles.buttonPressZone}></div>
       </div>
+    </button>
+    <div className={`${styles.buttonIndicator} ${isActive ? styles.active : ''}`} />
     </div>
   )
 }
