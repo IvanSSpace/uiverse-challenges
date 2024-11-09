8e36eed0b8f09f30297a78f21e090152f46d28ca
ivanShar <oldrum75@gmail.com>
2024-11-10 04:33:14 +0600

    wip: switch button to checkbox

--- src/keyboardButton/KeyboarButton.tsx
+++ src/keyboardButton/KeyboarButton.tsx
@@ -1,29 +1,31 @@
 import styles from './KeyboardButton.module.scss'
-import { useState } from 'react'
 
 const KeyboardButton = () => {
-  const [isActive, setIsActive] = useState(false)
-
-  const handleClick = () => {
-    setIsActive(!isActive)
-  }
-
   return (
     <div className={styles.buttonContainerAll}>
-      <button title="i made it for 7 hours" className={`${styles.buttonContainer} ${isActive ? styles.active : ''}`} onClick={handleClick}>
-        <div className={`${styles.button} ${isActive ? styles.active : ''}`}>
-          <div className={styles.buttonPressZone}>
-            <div className={styles.buttonImageContainer}>
-              <div className={styles.buttonImage}>
-                <svg width="36px" height="36px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M7 17L17 7L12 2V22L17 17L7 7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
+      <label>
+        <input title="i made it for 7 hours" type="checkbox" className={styles.hiddenCheckbox} />
+        <div className={styles.buttonContainer}>
+          <div className={styles.button}>
+            <div className={styles.buttonPressZone}>
+              <div className={styles.buttonImageContainer}>
+                <div className={styles.buttonImage}>
+                  <svg width="36px" height="36px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M7 17L17 7L12 2V22L17 17L7 7"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </button>
-      <div className={`${styles.buttonIndicator} ${isActive ? styles.active : ''}`} />
+        <div className={styles.buttonIndicator} />
+      </label>
     </div>
   )
 }
